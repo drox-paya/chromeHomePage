d1634edf3b90be7dea187f0970dac82a4b7b685c
Braiden <braidenwebb@outlook.com>
2024-05-27 22:45:33 +1000

    updates :)

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $darkModeText = $(".darkModeText");
+    console.log($darkModeText);
+    for (let i = 0; i < $darkModeText.length; i++){
+        
+    }
+        
+    
+});
