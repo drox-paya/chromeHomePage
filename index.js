2931c8770cafc00706498e440c5e7a34250762cd
Braiden <braidenwebb@outlook.com>
2024-06-01 20:46:55 +1000

    added themes :)

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-$(document).ready(function () {
-    $darkModeText = $(".darkModeText");
-    console.log($darkModeText);
-    for (let i = 0; i < $darkModeText.length; i++){
-        
+let currentTheme = "dark";
+function switchTheme() {
+    if (currentTheme === "dark") {
+        currentTheme = "light";
+        console.log("test");
+    } else {
+        currentTheme = "dark";
     }
-        
+    document.getElementById("bodyTheme").setAttribute("data-theme", currentTheme);
     
-});
+}
+function getTheme() {
+    return currentTheme;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+    console.log("DOM Loaded");
+    let daynightButton = document.getElementsByClassName("dayNightButton");
+    let dayNightButtonArray =  Array.from(daynightButton);
+
+    console.log(daynightButton)
+    dayNightButtonArray.forEach((element) => element.addEventListener("click", function() {
+        switchTheme();
+    }))
+
+
+
+
+})
