25fa532327db9bc5ca3e0627206482cd3f28d2a5
drox <50802693+drox-paya@users.noreply.github.com>
2024-06-07 17:14:17 +1000

    refactor: Update link icons and form action in index.html

--- index.js
+++ index.js
@@ -12,13 +12,68 @@ function switchTheme() {
 function getTheme() {
     return currentTheme;
 }
+function addCurrentLinksToStorage() {
+    let links = document.getElementsByClassName("iconContainer");
+    let linksArrayString = '';
+    let linksArray = Array.from(links);
+    for (let i = 0; i < linksArray.length; i++) {
+        let id = i;
+        let link = linksArray[i].children[1].href;;
+        let linkName = linksArray[i].children[1].innerHTML;
+        let linkIcon = linksArray[i].children[0].src;
+        console.log(" " + link + " " + linkName + " " + linkIcon);
+        // create an array item out of this
+        let linkArrayItem = {
+            "id": id,
+            "link": link,
+            "linkName": linkName,
+            "linkIcon": linkIcon
 
-function getLinksFromStorage() {
+        }
+        linksArrayString += JSON.stringify(linkArrayItem) + " ";
+    }
+   
+    localStorage.setItem("links", linksArrayString);
+}
     
+function getLinksFromStorage() {
+    let links = localStorage.getItem("links");
+    let linksArray = links.split(" ");
+    linksArray.pop();
+    for (let i = 0; i < linksArray.length; i++) {
+        let link = JSON.parse(linksArray[i]);
+        let linkContainer = document.getElementsByClassName("container")[0];
+        console.log(link);
+    }
+    console.log(linksArray);
     
 }
 function addNewLink () {
+    let linkContainer = document.getElementsByClassName("container")[0];
+    let google = document.getElementById("google");
+    let newLink = document.createElement("div");
+    newLink.classList.add("iconContainer");
+    let newLinkIcon = document.createElement("img");
+    newLinkIcon.classList.add("linkIcon");
+    
+    let newLinkAnchor = document.createElement("a");
+    newLinkIcon.append(newLinkAnchor);
+    newLink.append(newLinkIcon);
+    linkContainer.insertBefore(newLink, google);
 
+}
+function removeLink(id) {
+    let linkContainer = document.getElementsByClassName("container")[0];
+    let link = document.getElementById("link"+id);
+    linkContainer.removeChild(link);
+    // change links around to follow the id of links
+    let links = document.getElementsByClassName("iconContainer");
+    let linksArray = Array.from(links);
+    for (let i = 0; i < linksArray.length; i++) {
+        linksArray[i].id = "link"+i;
+    }
+
+    
 }
 
 
